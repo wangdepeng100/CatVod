04825333c74bb4f647abd97c5542627685639b8d
wangdepeng100 <208413002@qq.com>
2024-11-27 09:54:13 +0800

    更新 hezi.js

--- nodejs/src/spider/video/hezi.js
+++ nodejs/src/spider/video/hezi.js
@@ -7,6 +7,7 @@ import {load}from 'cheerio';
 let siteUrl = 'http://m.ttvbox.com';
 let headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 14; 22127RK46C Build/UKQ1.230804.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/118.0.0.0 Mobile Safari/537.36',
+    'Connection': 'keep-alive',
 };
 
 async function request(reqUrl, postData, post) {
@@ -15,7 +16,6 @@ async function request(reqUrl, postData, post) {
         headers: headers,
         data: postData || {},
         postType: post ? 'form' : '',
-        timeout:6000,
     });
     return res.data;
 }

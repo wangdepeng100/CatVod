5b5ea6669626d1690efd50d92801f43cfbfbea93
wangdepeng100 <208413002@qq.com>
2024-12-06 22:16:55 +0800

    更新 jihehe.js

--- nodejs/src/spider/video/jihehe.js
+++ nodejs/src/spider/video/jihehe.js
@@ -1,6 +1,6 @@
 import req from '../../util/req.js';
 import {load}from 'cheerio';
-import {ua, init, detail0, proxy, play, test, isEmpty}from '../../util/pan.js';
+import {ua, init, detail as _detail, proxy, play, test, isEmpty}from '../../util/pan.js';
 import dayjs from 'dayjs';
 
 let url = 'https://so.jihepan.top';
@@ -61,7 +61,7 @@ async function detail(inReq, _outResp) {
         let vod = ({
             vod_id: id,
         });
-        videos.push(await detail0(shareUrls, vod));
+        videos.push(await _detail(shareUrls, vod));
     }
     return {
         list: videos,

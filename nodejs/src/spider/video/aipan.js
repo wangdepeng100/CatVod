74992798978670917cc9e7015ffa86acb7338095
wangdepeng100 <208413002@qq.com>
2024-12-06 22:25:25 +0800

    更新 aipan.js

--- nodejs/src/spider/video/aipan.js
+++ nodejs/src/spider/video/aipan.js
@@ -1,6 +1,6 @@
 import req from '../../util/req.js';
 import { load } from 'cheerio';
-import { Apic, Qpic, ua, init ,detail0 ,proxy ,play ,test ,isEmpty } from '../../util/pan.js';
+import { Apic, Qpic, ua, init ,detail as _detail ,proxy ,play ,test ,isEmpty } from '../../util/pan.js';
 import dayjs from 'dayjs';
 
 let siteUrl = 'https://www.aipanso.com';
@@ -40,7 +40,7 @@ async function detail(inReq, _outResp) {
         let vod = ({
             vod_id: id,
         });
-        videos.push(await detail0(shareUrl ,vod));
+        videos.push(await _detail(shareUrl ,vod));
         return {
             list: videos,
         };

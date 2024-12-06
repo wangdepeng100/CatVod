84686a73dc73d9e6e581d2b4dd789f37aa40c856
wangdepeng100 <208413002@qq.com>
2024-12-06 22:14:24 +0800

    更新 cm.js

--- nodejs/src/spider/video/cm.js
+++ nodejs/src/spider/video/cm.js
@@ -1,6 +1,6 @@
 import req from '../../util/req2.js';
 import { load } from 'cheerio';
-import { ua, init ,detail0 ,proxy ,play ,test } from '../../util/pan.js';
+import { ua, init ,detail as _detail ,proxy ,play ,test } from '../../util/pan.js';
 import dayjs from 'dayjs';
 
 let url = 'https://tudou.lvdoui.top';
@@ -153,7 +153,7 @@ async function detail(inReq, _outResp) {
         const shareUrls = $('div.module-row-info p')
             .map((_, p) => p.children[0].data)
             .get();
-        videos.push(await detail0(shareUrls ,vod));
+        videos.push(await _detail(shareUrls ,vod));
     }
     return {
         list: videos,

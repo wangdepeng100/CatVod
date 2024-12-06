26150c01b948bad7f8e3b050d71b174a47b2f0bd
wangdepeng100 <208413002@qq.com>
2024-12-06 22:20:59 +0800

    更新 mipan.js

--- nodejs/src/spider/video/mipan.js
+++ nodejs/src/spider/video/mipan.js
@@ -1,6 +1,6 @@
 import req from '../../util/req.js';
 import {load}from 'cheerio';
-import { Apic, Qpic, ua, init ,detail0 ,proxy ,play ,test ,isEmpty } from '../../util/pan.js';
+import { Apic, Qpic, ua, init ,detail as _detail ,proxy ,play ,test ,isEmpty } from '../../util/pan.js';
 import dayjs from 'dayjs';
 
 let url = 'https://www.misou.fun';
@@ -24,7 +24,7 @@ async function detail(inReq, _outResp) {
         let vod = ({
             vod_id: id,
         });
-        videos.push(await detail0(shareUrls, vod));
+        videos.push(await _detail(shareUrls, vod));
     }
     return {
         list: videos,
